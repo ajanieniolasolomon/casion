a13746d120cf5046ab7bce886da2472fcd06da41
ajanieniolasolomon <eniolsolomon@gmail.com>
2025-06-30 04:19:47 -0700

    PaymentServicsexx

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Enable CORS for Angular frontend
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    credentials: true,
-  });
+
 
   // Swagger setup
   const config = new DocumentBuilder()
